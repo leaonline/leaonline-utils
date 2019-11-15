3fa9cfabac994d7c26e70dd07eaa2a1f3eb13072
Jan Kuester <jkuester@uni-bremen.de>
2019-11-15 09:59:24 +0100

    standard lint fix

--- package.js
+++ package.js
@@ -1,3 +1,4 @@
+/* eslint-env meteor */
 Package.describe({
   name: 'leaonline:utils',
   version: '1.0.0',
@@ -8,17 +9,17 @@ Package.describe({
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
-});
+})
 
-Package.onUse(function(api) {
-  api.versionsFrom('1.8.1');
-  api.use('ecmascript');
-  api.mainModule('utils.js');
-});
+Package.onUse(function (api) {
+  api.versionsFrom('1.8.1')
+  api.use('ecmascript')
+  api.mainModule('utils.js')
+})
 
-Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('leaonline:utils');
-  api.mainModule('utils-tests.js');
-});
+Package.onTest(function (api) {
+  api.use('ecmascript')
+  api.use('tinytest')
+  api.use('leaonline:utils')
+  api.mainModule('utils-tests.js')
+})
--- utils-tests.js
+++ utils-tests.js
@@ -1,11 +1,11 @@
 // Import Tinytest from the tinytest Meteor package.
-import { Tinytest } from "meteor/tinytest";
+import { Tinytest } from 'meteor/tinytest'
 
 // Import and rename a variable exported by utils.js.
-import { name as packageName } from "meteor/leaonline:utils";
+import { name as packageName } from 'meteor/leaonline:utils'
 
 // Write your tests here!
 // Here is an example.
 Tinytest.add('utils - example', function (test) {
-  test.equal(packageName, "utils");
-});
+  test.equal(packageName, 'utils')
+})
